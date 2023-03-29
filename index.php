7f6b1c982fd17fd069d3f2c9de3e2e855d972776
paco0410 <>
2023-03-29 13:56:55 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+       $nom = "Frsncisco";
+       $calle = "Hidalgo";
+       $no = "7";
+       $colonia = "Pablo";https://act17.paco0410.repl.co
+       $municipio = "Saltillo";
+       $cel = "8448525882";
+       echo "Me llamo $nom  <br>";
+       echo "Vivo en la calle $calle <br>";
+       echo " estoy en la colonia $colonia <br>";
+       echo " el numero de mi casa es $no <br>";
+       echo "y vivo en la ciudad $municipio ,<br> ";
+       echo " mi numero de celular es $cel<br> ";
+    ?>
+  </body>
+</html>
